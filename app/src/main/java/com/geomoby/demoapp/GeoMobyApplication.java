b4787c896ff2afa5b5e454154c9c383a659f92ea
Bohdan <bogdan.rachok@gmail.com>
2019-11-28 14:33:09 +0200

    Fix notifications

--- app/src/main/java/com/geomoby/demoapp/GeoMobyApplication.java
+++ app/src/main/java/com/geomoby/demoapp/GeoMobyApplication.java
@@ -7,20 +7,12 @@ import androidx.multidex.MultiDexApplication;
 public class GeoMobyApplication extends MultiDexApplication {
 
     private static Context mContext;
-    // private GeoMobyStateReceiver geoMobyStateReceiver = new GeoMobyStateReceiver();
 
     @Override
     public void onCreate() {
         super.onCreate();
         mContext = getApplicationContext();
 
-       /* IntentFilter filter = new IntentFilter();
-        filter.addAction("BroadcastInitLocation");
-        filter.addAction("GEOMOBY_NEW_DISTANCE");
-        filter.addAction("GEOMOBY_BEACON_SCAN");
-        filter.addAction("GEOMOBY_NEW_FENCE_LIST");
-
-        getApplicationContext().registerReceiver(geoMobyStateReceiver, filter);*/
     }
 
     public static Context getContext() {
